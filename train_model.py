9c6ed70916f62224d465f642e38a250c056b1e45
Mike Corsner <mcorsne@wgu.edu>
2026-09-14 19:51:21 -0400

    Add FastAPI inference endpoints and local API test

--- train_model.py
+++ train_model.py
@@ -12,14 +12,11 @@ from ml.model import (
     save_model,
     train_model,
 )
-# TODO: load the cencus.csv data
 project_path = os.path.dirname(os.path.abspath(__file__))
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
 data = pd.read_csv(data_path)
 
-# TODO: split the provided data to have a train dataset and a test dataset
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(
     data,
     test_size=0.20,
@@ -38,7 +35,6 @@ cat_features = [
     "native-country",
 ]
 
-# TODO: use the process_data function provided to process the data.
 X_train, y_train, encoder, lb = process_data(
     train,
     categorical_features=cat_features,
@@ -55,7 +51,6 @@ X_test, y_test, _, _ = process_data(
     lb=lb,
 )
 
-# TODO: use the train_model function to train the model on the training dataset
 model = train_model(X_train, y_train)
 
 # save the model and the encoder
@@ -69,14 +64,12 @@ model = load_model(
     model_path
 )
 
-# TODO: use the inference function to run the model inferences on the test dataset.
 preds = inference(model, X_test)
 
 # Calculate and print the metrics
 p, r, fb = compute_model_metrics(y_test, preds)
 print(f"Precision: {p:.4f} | Recall: {r:.4f} | F1: {fb:.4f}")
 
-# TODO: compute the performance on model slices using the performance_on_categorical_slice function
 
 open("slice_output.txt", "w").close()
 
